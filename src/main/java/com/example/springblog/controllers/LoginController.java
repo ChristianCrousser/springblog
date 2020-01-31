28d1ef0b4d73b5025dcaafa32d1deec6f40391cc
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-01-31 13:58:15 -0600

    maven dependency added

--- src/main/java/com/example/springblog/controllers/LoginController.java
+++ src/main/java/com/example/springblog/controllers/LoginController.java
@@ -1,4 +1,40 @@
 package com.example.springblog.controllers;
 
+
+import com.example.springblog.models.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class LoginController {
+
+//        @GetMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+
+    public String GetLoginForm() {
+        return "login";
+    }
+
+//    @GetMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+
+    public String login(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
+
+        String username = loginForm.getUsername();
+        String password = loginForm.getPassword();
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            return "home";
+        }
+
+        model.addAttribute("invalidCredintials", true);
+
+        return "login";
+
+
+    }
 }
