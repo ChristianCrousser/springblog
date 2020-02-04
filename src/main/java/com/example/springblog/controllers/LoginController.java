337d1dc5ee6c58ee7dbf68e37be4ad625bf65ba7
ChristianCrousser <christian.crousserkaiman@gmail.com>
2020-02-04 15:21:06 -0600

    changed password

--- src/main/java/com/example/springblog/controllers/LoginController.java
+++ src/main/java/com/example/springblog/controllers/LoginController.java
@@ -4,15 +4,13 @@ package com.example.springblog.controllers;
 import com.example.springblog.models.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
 
 //        @GetMapping("/login")
+//        @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.GET)
 
     public String GetLoginForm() {
@@ -20,6 +18,7 @@ public class LoginController {
     }
 
 //    @GetMapping("/login")
+//    @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.POST)
 
     public String login(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
